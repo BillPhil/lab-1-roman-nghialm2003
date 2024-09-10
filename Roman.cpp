57711e499749c0ae4e0dd1f525b14410499d96b4
William Wyatt Phillips <phillips4@phys-k119680.local>
2024-09-09 20:20:02 -0400

    defined public in Roman.cpp

--- Roman.cpp
+++ Roman.cpp
@@ -4,9 +4,39 @@
 #include <cstring>
 #include "Roman.h"
 
+
+
+
+
+
+
+
+
+
+
 // Default constructor
 Roman::Roman() : romanNumeral("I"), integerValue(1) {}
 
 // Parameterized constructor
 Roman::Roman(const std::string& numeral) : romanNumeral(numeral) {}
 Roman::Roman(int value) : integerValue(value) {}
+
+//Print Integer of numeral
+void Roman::printDec()const {
+    std::cout << integerValue << std::endl;
+}
+
+//Print Integer of numeral
+void Roman::printRoman()const {
+    std::cout << romanNumeral << std::endl;
+}
+
+//Return integer
+int Roman::getInt() const {
+    return integerValue;
+}
+
+//Return Roman Numeral string
+std::string Roman::getRoman() const {
+    return romanNumeral;
+}
